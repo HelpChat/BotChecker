e11da44210b16a81435a59b318afd14be8aaef10
PiggyPiglet <noreply@piggypiglet.me>
2019-02-19 18:00:59 +0800

    Console Command abstract class
    I don't know why this wasn't in the previous commit

--- src/main/java/me/piggypiglet/botchecker/core/framework/commands/ConsoleCommand.java
+++ src/main/java/me/piggypiglet/botchecker/core/framework/commands/ConsoleCommand.java
@@ -1,8 +1,15 @@
 package me.piggypiglet.botchecker.core.framework.commands;
 
 // ------------------------------
-// Copyright (c) PiggyPiglet 2018
+// Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
-public final class ConsoleCommand {
+public abstract class ConsoleCommand {
+    private final String[] commands;
+
+    protected ConsoleCommand(String... commands) {
+        this.commands = commands;
+    }
+
+    protected abstract void execute(String[] args);
 }
